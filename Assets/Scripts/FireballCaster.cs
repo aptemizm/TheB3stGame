0d1988a5c7965e33b3cd6c866d478de72c0f9dcf
aptemizm <spskuratov@gmail.com>
2024-02-29 23:29:20 +0300

    new comit

---
+++ Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public Fireball fireballPrefab;
+    public Transform fireballSourceTransform;
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
+    }
+}
